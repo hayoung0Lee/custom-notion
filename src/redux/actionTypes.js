a3b6c6d62170779d0065175a3f290d62f7c23069
hayoung0Lee <hayoung0.lee@gmail.com>
2021-05-26 21:33:01 +0900

    actionTypes, and actions prototype

---
+++ src/redux/actionTypes.js
@@ -0,0 +1,9 @@
+// page
+export const ADD_PAGE = "ADD_PAGE";
+export const REMOVE_PAGE = "REMOVE_PAGE";
+export const EDIT_PAGE = "EDIT_PAGE";
+
+// block
+export const ADD_BLOCK = "ADD_BLOCK";
+export const REMOVE_BLOCK = "REMOVE_BLOCK";
+export const EDIT_BLOCK = "EDIT_BLOCK";
---
+++ src/redux/actions.js
@@ -0,0 +1,20 @@
+import {
+  ADD_PAGE,
+  REMOVE_PAGE,
+  EDIT_PAGE,
+  ADD_BLOCK,
+  REMOVE_BLOCK,
+  EDIT_BLOC,
+} from "./actionTypes";
+
+let nextPageId = 0;
+
+export const addPage = (pageName) => ({
+  type: ADD_PAGE,
+  payload: { pageName, blocks: [] },
+});
+
+export const addBlock = (pageId, isRoot, contents) => ({
+  type: ADD_BLOCK,
+  payload: { pageId, isRoot, contents },
+});
