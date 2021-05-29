aedf2c9d763da87d8b85103224c416cce6d6725b
hayoung0Lee <hayoung0.lee@gmail.com>
2021-05-29 15:35:14 +0900

    Focus on the last element

--- src/components/EditableBlock.js
+++ src/components/EditableBlock.js
@@ -1,5 +1,5 @@
 import ContentEditable from "react-contenteditable";
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useContext } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getBlockState, getCurrentBlockInfo } from "../redux/selectors";
 import { updateCurrentBlock } from "../redux/actions";
@@ -9,6 +9,7 @@ const EditableBlock = ({
   depth, // tab횟수 제한용
   blockId, // 현재의 blockId
   parentId, // 어디에서 현재 블록을 호출했는지를 체크한다, -1은 root에서 불렀단 것
+  isLast, //  한 loop의 마지막 노드에는 일단 마지막일지도 모르니까 isLast를 true로 넘긴다.
 }) => {
   const dispatch = useDispatch();
   const blockInfo = useSelector((state) => getCurrentBlockInfo(state, blockId));
@@ -16,44 +17,12 @@ const EditableBlock = ({
 
   const [blockValue, setBlockValue] = useState(blockInfo.contents);
 
-  // const onKeyDownHandler = (e, pageId, isRoot, blockId, previousBlock) => {
-  //   if (e.key === "Enter") {
-  //     e.preventDefault();
-  //     if (addBlock) {
-  //       addBlock(
-  //         ref,
-  //         isRoot ? pageId : -1,
-  //         isRoot,
-  //         isRoot ? -1 : blockId,
-  //         ref.current.dataset.parentId
-  //       );
-  //     }
-  //     return;
-  //   } else if (e.key === "Tab") {
-  //     e.preventDefault();
-
-  //     if (depth >= 3 || !previousBlock) {
-  //       return;
-  //     }
-
-  //     dispatch(addTab(pageId, isRoot, previousBlock.dataset.blockId, blockId));
-  //     return;
-  //   }
-  // };
-
-  // const onBlurHandler = () => {
-  //   if (blockInfo.contents.trim() !== ref.current.innerText.trim()) {
-  //     dispatch(updateCurrentBlock(blockId, ref.current.innerText));
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (blockInfo.blocks.length > 0) {
-  //     // FIXME: focusing이거 제일 마지막에 되는것말고, 중간에 Block을 추가하더라도 되도록 만들어야함
-  //     mainRef.current.lastElementChild.focus();
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [blockInfo.blocks]);
+  useEffect(() => {
+    // isLast === true이면 마지막일지도 모르는것
+    if (isLast && blockInfo.blocks.length === 0) {
+      ref.current.focus();
+    }
+  }, []);
 
   return (
     <div data-block-id={blockId} data-parent-id={parentId}>
@@ -63,17 +32,21 @@ const EditableBlock = ({
         disabled={false}
         onChange={(e) => setBlockValue(e.target.value)}
         className={`my-5 p-2 w-3/4 hover:bg-gray-100`}
+        tabindex="-1"
       />
       {/* subeditable */}
       {blockInfo.blocks.length > 0 && (
         <div className="ml-3" data-parent-id={blockId}>
-          {blockInfo.blocks.map((subBlockId) => {
+          {blockInfo.blocks.map((subBlockId, index) => {
             return (
               <EditableBlock
-                key={blockId}
+                key={subBlockId}
                 blockId={subBlockId}
                 parentId={blockId}
                 depth={depth + 1}
+                isLast={
+                  isLast === true && index === blockInfo.blocks.length - 1
+                } // 마지막일지도 모르는 중에서도 마지막인것들한테만 true를 넘겨준다
               />
             );
           })}
--- src/components/Main.js
+++ src/components/Main.js
@@ -13,16 +13,16 @@ const Main = ({ pageId }) => {
   });
 
   const rootBlock = useSelector((state) => getRootBlocks(state, pageId));
-
   return (
     <main className="col-span-10 bg-pink-100 min-h-screen p-3.5" ref={mainRef}>
-      {rootBlock.map((blockId) => {
+      {rootBlock.map((blockId, index) => {
         return (
           <EditableBlock
             key={blockId}
             depth={1} // tab횟수 제한용
             blockId={blockId} // 현재의 blockId
             parentId={-1} // -1은 root에서 호출했다는 뜻
+            isLast={index === rootBlock.length - 1} // 한 loop의 마지막 노드에는 일단 마지막일지도 모르니까 isLast를 true로 넘긴다.
           />
         );
       })}
