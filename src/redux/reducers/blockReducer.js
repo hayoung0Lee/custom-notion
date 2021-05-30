4d908827ab01b8ee01805276861c55ee70bd4e26
hayoung0Lee <hayoung0.lee@gmail.com>
2021-05-30 16:23:21 +0900

    Fix reordering bug

--- src/redux/reducers/blockReducer.js
+++ src/redux/reducers/blockReducer.js
@@ -116,27 +116,28 @@ const blockReducer = (state = initialState, action) => {
       const { pageId, parentId, sourceIndex, destinationIndex } =
         action.payload;
 
-      const a = state.blockById[parentId]?.blocks[sourceIndex];
-      const b = state.blockById[parentId]?.blocks[destinationIndex];
-
-      if (parentId === -1 || !a || !b) {
+      if (parentId === -1) {
         return state;
       } else {
+        const sourceValue = state.blockById[parentId]?.blocks[sourceIndex];
+
+        const newOrder = [...state.blockById[parentId].blocks];
+        if (sourceIndex < destinationIndex) {
+          // 원래 위치보다 뒤로 추가
+          newOrder.splice(destinationIndex + 1, 0, sourceValue);
+          newOrder.splice(sourceIndex, 1);
+        } else if (sourceIndex > destinationIndex) {
+          // 원래 위치보다 앞에 추가
+          newOrder.splice(destinationIndex, 0, sourceValue);
+          newOrder.splice(sourceIndex + 1, 1);
+        }
         return {
           ...state,
           blockById: {
             ...state.blockById,
             [parentId]: {
               ...state.blockById[parentId],
-              blocks: state.blockById[parentId].blocks.map((block, index) => {
-                if (index !== sourceIndex && index !== destinationIndex) {
-                  return block;
-                }
-                if (index === sourceIndex) {
-                  return b;
-                }
-                return a;
-              }),
+              blocks: newOrder,
             },
           },
         };
--- src/redux/reducers/pageReducer.js
+++ src/redux/reducers/pageReducer.js
@@ -67,27 +67,28 @@ const pageReducer = (state = initialState, action) => {
     case REORDER_BLOCK: {
       const { pageId, parentId, sourceIndex, destinationIndex } =
         action.payload;
-      const a = state.pageById[pageId].blocks[sourceIndex];
-      const b = state.pageById[pageId].blocks[destinationIndex];
 
       if (parentId === -1) {
+        const sourceValue = state.pageById[pageId].blocks[sourceIndex];
+
+        const newOrder = [...state.pageById[pageId].blocks];
+
+        if (sourceIndex < destinationIndex) {
+          // 원래 위치보다 뒤로 추가
+          newOrder.splice(destinationIndex + 1, 0, sourceValue);
+          newOrder.splice(sourceIndex, 1);
+        } else if (sourceIndex > destinationIndex) {
+          // 원래 위치보다 앞에 추가
+          newOrder.splice(destinationIndex, 0, sourceValue);
+          newOrder.splice(sourceIndex + 1, 1);
+        }
         return {
           ...state,
           pageById: {
             ...state.pageById,
             [pageId]: {
               ...state.pageById[pageId],
-              blocks: state.pageById[pageId].blocks.map((block, index) => {
-                if (index !== sourceIndex && index !== destinationIndex) {
-                  return block;
-                }
-
-                if (index === sourceIndex) {
-                  return b;
-                }
-
-                return a;
-              }),
+              blocks: newOrder,
             },
           },
         };
